659a099f041555a20f86c12f03e4f5c48fcaa004
Chett Tiller <chett.tiller@gmail.com>
2018-09-26 20:17:54 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
-def alphabetize(arr)
-  # code here
+sample = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+ESPERANTO = ["a","b","c","ĉ","d","e","f","g","ĝ","h","ĥ","i","j","ĵ","k","l","m","n","o","p","r","s","ŝ","t",
+  "u","ŭ","v","z"]
+
+def alphabetize(phrases)
+  phrases.sort_by!{ |a| ESPERANTO.index(a.split("").first) }
+  puts phrases
+  phrases
 end
+
+alphabetize(sample)
